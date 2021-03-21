a74e4b9eb31f8eea7962bb037759fde8a600f3a5
Jeffrey Lee Li <Jeffrey.Lee.Li@qq.com>
2021-03-21 16:38:04 +0800

    implement command scheme

--- src/doku-cli/main.go
+++ src/doku-cli/main.go
@@ -1,9 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	d := NewDoKuData(4, 5, 2)
-	d.Init()
-	fmt.Println(d)
+
+	cmd := `h`
+	for {
+		fmt.Printf(`[%s] `, cmd)
+
+		fmt.Scanln(&cmd)
+
+		switch strings.ToLower(cmd) {
+		case `n`, `new`:
+			{
+				d.Init()
+				fmt.Println(d)
+			}
+		case `h`, `help`:
+			{
+				displayKeymap()
+			}
+		case `q`, `quit`:
+			{
+				fmt.Println("Seeya~")
+				return
+			}
+		default:
+			{
+				fmt.Println("Invalid Command:", cmd)
+			}
+		}
+	}
+}
+
+func displayKeymap() {
+	fmt.Println(`[N]ew | W: up S: down A: left D: right | [H]elp [Q]uit`)
 }
