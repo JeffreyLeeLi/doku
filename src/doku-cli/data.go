e07bf26c199c745fcd5ff6246a9176b771c62692
Jeffrey Lee Li <Jeffrey.Lee.Li@qq.com>
2021-03-21 12:13:48 +0800

    implement Stringer interface

--- src/doku-cli/data.go
+++ src/doku-cli/data.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type DoKuData struct {
 	Data [][]int
 }
@@ -14,4 +16,29 @@ func NewDoKuData(n, l int) *DoKuData {
 	return &DoKuData{
 		Data: data,
 	}
+}
+
+func (p *DoKuData) String() string {
+	s := ""
+
+	n := len(p.Data)
+
+	for i := 0; i < n; i++ {
+		if i > 0 {
+			s += "\n"
+		}
+
+		t := p.Data[i]
+		l := len(t)
+
+		for j := 0; j < l; j++ {
+			if j > 0 {
+				s += " "
+			}
+
+			s += fmt.Sprint(t[j])
+		}
+	}
+
+	return s
 }
