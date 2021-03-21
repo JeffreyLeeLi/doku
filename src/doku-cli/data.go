9739b858f8458d6999c515eaf1fddd29200914e7
Jeffrey Lee Li <Jeffrey.Lee.Li@qq.com>
2021-03-21 12:13:22 +0800

    implement construction func

--- src/doku-cli/data.go
+++ src/doku-cli/data.go
@@ -3,3 +3,15 @@ package main
 type DoKuData struct {
 	Data [][]int
 }
+
+func NewDoKuData(n, l int) *DoKuData {
+	data := make([][]int, n)
+
+	for i := 0; i < n; i++ {
+		data[i] = make([]int, l)
+	}
+
+	return &DoKuData{
+		Data: data,
+	}
+}
